138ef1d5be0fa4f67f23dbb6e043dd6bf17aaf3f
danialdo95 <danialdo95@gmail.com>
2018-11-01 23:47:27 +0800

    Added pug templating engine

--- routes/admin.js
+++ routes/admin.js
@@ -6,16 +6,22 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product',(req,res, next) => {
     // console.log('In another the middleware');
     res.sendFile(path.join(rootDir, 'views','add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    //console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 
 });
 
-module.exports = router;
+//module.exports = router;
+
+exports.routes = router; 
+exports.products = products;
 
