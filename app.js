caa65b2eba33e35925a50794649cbad376bf148c
danialdo95 <danialdo95@gmail.com>
2019-07-03 00:35:29 +0800

    Added ejs templating engine

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const path = require('path');
 
 const express = require('express');
+
+//Body parser is to parse request and response body 
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars')
+
+//Use for setting up handlebars 
+//const expressHbs = require('express-handlebars')
 
 const app = express();
 
+//Added for environment port deploying in heroku
 const PORT = process.env.PORT || 5000
 
-app.engine(
-    'hbs', 
-    expressHbs({
-        layoutsDir: 'views/layouts/', 
-        defaultLayout: 'main-layout',
-        extname: 'hbs'
-    })
-);
+//for handlebars need to set up view engine
+// app.engine(
+//     'hbs', 
+//     expressHbs({
+//         layoutsDir: 'views/layouts/', 
+//         defaultLayout: 'main-layout',
+//         extname: 'hbs'
+//     })
+// );
+
+
 // app.set('view engine', 'pug');
-app.set('view engine', 'hbs');
+//app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
