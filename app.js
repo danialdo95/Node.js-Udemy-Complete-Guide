73a8b3073db6c8f6e6c47793027886ba650b7007
danialdo95 <danialdo95@gmail.com>
2019-07-07 01:32:27 +0800

    Refactored code to MVC pattern, products data in json file, and enhance features

--- app.js
+++ app.js
@@ -5,42 +5,29 @@ const express = require('express');
 //Body parser is to parse request and response body 
 const bodyParser = require('body-parser');
 
-//Use for setting up handlebars 
-//const expressHbs = require('express-handlebars')
+const errorController = require('./controllers/error')
 
 const app = express();
 
 //Added for environment port deploying in heroku
 const PORT = process.env.PORT || 5000
 
-//for handlebars need to set up view engine
-// app.engine(
-//     'hbs', 
-//     expressHbs({
-//         layoutsDir: 'views/layouts/', 
-//         defaultLayout: 'main-layout',
-//         extname: 'hbs'
-//     })
-// );
 
-
-// app.set('view engine', 'pug');
-//app.set('view engine', 'hbs');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes); 
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found'});
-});
+app.use(errorController.get404Page);
 
 
 app.listen(PORT);
--- routes/shop.js
+++ routes/shop.js
@@ -5,23 +5,19 @@ const express = require('express');
 const rootDir = require('../util/path');
 const adminData = require('./admin');
 
+const shopController =  require('../controllers/shop')
+
 
 const router = express.Router();
 
-router.get('/',(req,res, next) => {
-    //console.log('shop.js', adminData.products);
-    //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get('/', shopController.getIndex);
 
-    const products = adminData.products;
-    res.render('shop', 
-    {
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    });
-});
+router.get("/products", shopController.getProducts);
+
+router.get("/cart", shopController.getCart);
+
+router.get("/orders", shopController.getOrders);
+
+router.get("/checkout" , shopController.getCheckout);
 
 module.exports = router;
